ece333f0aa4a6548a23c5a76cd006a37cedc3e9b
Sam Radinfar <hhesamradinfar@gmail.com>
2025-02-03 11:43:18 +0100

    Ajouter les code javaScript

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,45 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+const bannerImg = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p");
+const dots = document.querySelectorAll(".dot")
+const prevBtn = document.querySelector(".arrow_left");
+const nextBtn = document.querySelector(".arrow_right");
+
+
+let = currentIndex = 0;
+
+
+function showSlide (index) {
+	bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+	bannerText.innerHTML = slides[index].tagLine;
+
+	dots.forEach((dot, i) => {
+		dot.classList.toggle('dot_selected' , i===index);
+	});
+}
+
+
+prevBtn.addEventListener("click", () => {
+	currentIndex = (currentIndex > 0) ? currentIndex - 1 : slides.length -1;
+	showSlide(currentIndex);
+});
+
+
+nextBtn.addEventListener("click", () => {
+	currentIndex = (currentIndex < slides.length -1) ? currentIndex + 1 : 0;
+	showSlide(currentIndex);
+});
+
+
+dots.forEach((dot, index) => {
+	dot.addEventListener("click", () => {
+		currentIndex = index;
+        showSlide(currentIndex);
+	})
+})
+
